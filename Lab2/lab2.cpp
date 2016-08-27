9adc46ed69b0f1b0e8c00b65b7b9fb25472956cf
Tim Torres <ttorres008@student.butte.edu>
2016-08-27 22:19:51 +0000

    Lab 2 Alogrithm for guessing game

---
+++ Lab2/lab2.cpp
@@ -0,0 +1,12 @@
+//Creating an Algorithm for a guessing game
+//Tim Torres
+//8-27-16
+
+Introduction
+Rules to the guessing game.
+Computer chooses random number between 1 and 10.
+User tries to guess computers number.
+If user guesses correctly, user wins and earns 10 points.
+If user guesses incorrectly, computer tells user higher or lower from the number chosen.
+User continues to guess computers number until they win, while losing 1 point for each incorrect answer.
+Computer asks user to play again?
