9a333d0db0bb1478da217bec809a47c8fc76690f
Tim Torres <ttorres008@student.butte.edu>
2016-08-27 21:39:17 +0000

    Lab 1 ASCII Art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,22 @@
+//ASCII Art Lab
+//Tim Torres
+//8-24-16
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "************" << endl;
+  cout << "*          *" << endl;
+  cout << "*  ||  ||  *" << endl;
+  cout << "*  ||  ||  *" << endl;
+  cout << "*    ==    *" << endl;
+  cout << "*    ==    *" << endl;
+  cout << "*  ||  ||  *" << endl;
+  cout << "*  ||  ||  *" << endl;
+  cout << "*          *" << endl;
+  cout << "************" << endl;
+
+
+    
+}
