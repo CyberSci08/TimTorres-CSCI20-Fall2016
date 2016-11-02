d79bb47bbbcc6b5ba5ab256aad15d977660fe927
Tim Torres <ttorres008@student.butte.edu>
2016-11-02 08:30:34 +0000

    Lab 16: Creating a parallel array using menu program

--- Lab16/lab16.cpp
+++ Lab16/lab16.cpp
@@ -1,7 +1,6 @@
 //Tim Torres
 //CSCI20 MW 9:00-11:15
-//Lab 15: Single Dimension Array, creating a menu for the user to interact with
-//I used sources from StackFlow, YouTube and cplusplus.com and zybooks
+//Lab 16: Parallel Arrays: Using menu program from lab 15 to output prices and inventory
 
 #include <iostream>
 #include <string>
@@ -11,20 +10,20 @@ using namespace std;
 const int NUM_ITEMS = 10;
 const string menuItem[NUM_ITEMS] = {"Swords", "Breastplates", "Leggings", "Shields", "Rings",
                                    "Braces", "Axes", "Clubs", "Helmets", "Talismans"};
-                                   
+
+const float menuPrice[NUM_ITEMS] = {100.00, 75.00, 50.00, 85.00, 65.00, 50.00, 100.00, 95.00, 100.00, 25.00};       //Money representing each item
+                                 
  
  void printItems(int userItems[NUM_ITEMS]);
  void printMenu();
  bool selectItem(int userItems[NUM_ITEMS]);
-  
-
+ double printTotal(int userItems[NUM_ITEMS]);
 
 int main(){
     
     bool udone = false;                               
     int i = 0;
     int userItems[NUM_ITEMS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};      //Initialized inventory for userItems
-    
     cout << "Welcome to the shop!" << endl;
     cout << endl;
    
@@ -36,6 +35,7 @@ int main(){
     
     printItems(userItems);
     
+    
     udone = selectItem(userItems);
     
     cout << endl;
@@ -45,30 +45,44 @@ int main(){
     
    return 0;
 }
+//Print total of players inventory
+double printTotal(int userItems[NUM_ITEMS]){
+    
+}
+
+
 
 //Items the user has
 void printItems(int userItems[NUM_ITEMS]){
+    float menuSum = 0.0;                        //create sum variable to keep record of user purchased items
     cout << "Your items are: " << endl;
-    
     for (int i = 0; i < NUM_ITEMS; ++i){
+        
         if (userItems[i] > 0){
             cout << userItems[i] << " x " << menuItem[i] << endl;
+            menuSum += menuPrice[i];
+            
+            
         }
+        
     }
+    cout << "Your total is: $ " << menuSum << endl;
     cout << endl;
 }
 
 //Items in the shop menu
 void printMenu(){
     cout << "Shop Items" << endl;
+    cout << endl;
     for (int i = 0; i < NUM_ITEMS; ++i){
-        cout << i + 1 << ". " << menuItem[i] << endl;
+        cout << i + 1 << ". " << menuItem[i] << "---$ " << menuPrice[i] << endl;
     }
     cout << endl;
+    
 }
 
 //User selects items from menu
-bool selectItem(int userItems[NUM_ITEMS]){        //A bool function because it determines what the function returns
+bool selectItem(int userItems[NUM_ITEMS]){        //A bool function because it returns true or false condition
     int userIn;
     cout << "What would you like to purchase from the menu?" << endl;
     cout << "Enter " << 1 << "-" << NUM_ITEMS << endl;
