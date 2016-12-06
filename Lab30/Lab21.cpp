61ab9f256bf1ba4cce72ca19abba0d502bd42cb8
Tim Torres <ttorres008@student.butte.edu>
2016-12-06 00:33:55 +0000

    Lab 30 make-up for lab21 seperate file

---
+++ Lab30/Lab21.cpp
@@ -0,0 +1,27 @@
+//Tim Torres
+//CSCI20 MW 9:00-11:15
+//Lab 21 Seperate File compilation make-up renamed Lab 30
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+#include "MyClass.h"
+
+/*
+ *  This class is a dummy class to help show how to
+ *  break our class into an interface and implementation
+ *  file. It will set a member variable to 1 on initialization
+ *  and output it using the Output() function.
+ */
+ 
+// Program starts here
+int main() {
+  // Create a MyClass Object
+  MyClass object;
+  // Call the output member function
+  object.Output();
+  // This ends our program
+  return 0;
+}
